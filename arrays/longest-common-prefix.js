2925cf7508dbc6e3058a87db97a9fc25044b5429
Bethany Ingraham <BethFIngraham@gmail.com>
2026-01-16 23:55:06 -0500

    Explanation and solution for longest-common-prefix

--- arrays/longest-common-prefix.js
+++ arrays/longest-common-prefix.js
@@ -36,22 +36,48 @@
  *  6. If there is no commonality, return an empty string
  *  7. Return longest prefix
  *
- * Time Complexity: 
- * Space Complexity: 
- *
+ * Time Complexity: O(S) Worst case - visit every char in every str
+ * Space Complexity: O(1) Same data structure - nothing extra
+ * 
+ * To Remember:
+ *  - Very fast and returns early if necessary
+ *  - indexOf(), substring(), and substring() vs slice()
+ *  - look into vertical solution (this is horizontal scanning)
  */
 function longestCommonPrefix(strs) {
+
+  // If array is empty, there is no prefix to return
   if(strs.length === 0) return ""
 
+  // If array contains one word, that is the longest prefix 
   if(strs.length === 1) return strs[0]
 
+  // The whole first word is set as the starting prefix
   let prefix = strs[0];
   
+  // (outer) loop through each word in array, starting with the second word
+  for(let i = 1; i < strs.length; i++) {
+
+    // (inner) loop while the current word does not start with prefix 
+        // this returns 0 if entire prefix string is found at 
+        // the beginning of the current word
+    while(strs[i].indexOf(prefix) !== 0) {
+
+        // cut off the last char in the prefix word
+        prefix = prefix.substring(0, prefix.length - 1);
+
+        // if no chars in prefix remain return empty str
+        if(prefix === "") return ""
+    }
+  }
+
+  // return whatever is left of prefix after checking every word
+  return prefix 
 }
 
-// console.log(longestCommonPrefix(["flower","flow","flight"])) // Expected: "fl"
-// console.log(longestCommonPrefix(["dog","racecar","car"])) // Expected: ""
-// console.log(longestCommonPrefix(["dog"])) // Expected: "dog"
-// console.log(longestCommonPrefix([])) // Expected: ""
+console.log(longestCommonPrefix(["flower","flow","flight"])) // Expected: "fl"
+console.log(longestCommonPrefix(["dog","racecar","car"])) // Expected: ""
+console.log(longestCommonPrefix(["dog"])) // Expected: "dog"
+console.log(longestCommonPrefix([])) // Expected: ""
 
 
