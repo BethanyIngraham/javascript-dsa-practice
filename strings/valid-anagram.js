a7c3123cfba92ec01448d1ce71d4348831354555
Bethany Ingraham <BethFIngraham@gmail.com>
2026-01-18 22:29:55 -0500

    More optimal solution for valid-anagram

--- strings/valid-anagram.js
+++ strings/valid-anagram.js
@@ -34,19 +34,19 @@
  * Approach:
  *  1. If both strings are empty return true
  *  2. If strings have different lengths return false
- *  3. Store the letters from the original string and their frequency
- *  4. Store the letters from the second string and their frequency
- *  5. Make comparison of both strings
- *  6. If they match completely, return true
- *  7. If not, return false
+ *  3. Store the letters from string s and their frequency in obj
+ *  4. Using string t, subtract letter occurence from s obj
+ *  5. If a letter does not exist in obj or more letters are found
+ *         return false
+ *  6. If anagram is found return true
  * 
  * Time Complexity: O(n) 
  * Space Complexity: O(1)
  * 
  * To Remember: 
  *  Frequency counter 
- *  How to compare basic value objects
- *    Compare content not reference
+ *  Optimization - Making use of one object for two purposes
+ *  Use of less memory and fewer loops
  * 
  * Follow up: 
  *  What if the inputs contain Unicode characters? 
@@ -54,6 +54,51 @@
  */
 function isAnagram(s,t) {
 
+  // if both strings are empty return true
+  if(s === "" && t === "") return true 
+
+  // if strings have different lengths return false
+  if(s.length !== t.length) return false
+
+  // object stores letters and their frequency of string s
+  const letterCount = {};
+
+  // loop through string s 
+  // if letter already exists in obj increase count by 1
+  // else set letter as key and its value at 1
+  for(let i = 0; i < s.length; i++) {
+    let char = s[i];
+   if(letterCount[char]) {
+    letterCount[char]++;
+   } else {
+    letterCount[char] = 1;
+   }
+  }
+
+  // loop through string t
+  // if a letter does not exist or is already at zero, meaning there
+  // is an extra letter in t - return false
+  // for every letter, decrement their value in the letterCount obj
+  for(let i = 0; i < t.length; i++) {
+    let char = t[i];
+    if(!letterCount[char] || letterCount[char] === 0) {
+        return false
+    }
+    letterCount[char]--; 
+  }
+
+  // the loop has finished and string t is an anagram of string s
+  return true
+}
+
+// console.log(isAnagram("anagram","nagaram")); // Expected: true
+// console.log(isAnagram("rat","car")); // Expected: false
+// console.log(isAnagram("hellogoodbye", "hello")); // Expected: false
+// console.log(isAnagram("", "")); // Expected: true
+
+/**
+ * Original Solution:
+ 
   // if both strings are empty return true
   if(s === "" && t === "") return true 
 
@@ -101,11 +146,4 @@ function isAnagram(s,t) {
 
   // if everything matches after the loop, letters and values, return true
   return true
-  
-}
-
-// console.log(isAnagram("anagram","nagaram")); // Expected: true
-// console.log(isAnagram("rat","car")); // Expected: false
-// console.log(isAnagram("hellogoodbye", "hello")); // Expected: false
-// console.log(isAnagram("", "")); // Expected: true
-
+ */
